216116b4077cd5ad15bb10e2e1f88c626bf502f9
Davian99 <davidanimal1999@hotmail.com>
2025-03-27 10:27:49 +0100

    Rewrite to allow generic paths (-)
    See new tests for more information

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const isJsonPointer = require('@walcu-engineering/isjsonpointer');
 const assert = require('assert');
+
 /**
  * This is a getIn equivalent
  * @param object: any value
@@ -8,16 +9,26 @@ const assert = require('assert');
  */
 const getPathValue = (object, path = '') => {
   assert(isJsonPointer(path), `${path} is not a JSON pointer path`);
-  return path
-    .split('/')
-    .slice(1)
-    .reduce((subpath_part_value, subpath_part, i, path_array) => {
-      if(subpath_part_value == null){
-        if(i < path_array.length - 1) return {};
-        return subpath_part_value;
+  const splitted_path = path.split('/');
+  const getPathValueRec = (obj, idx) => {
+    const curr_path = splitted_path[idx];
+    if (curr_path == null || obj == null) return obj;
+    if (curr_path === '-') {
+      if (!Array.isArray(obj))
+        throw new TypeError(obj + ' is not an array');
+      let ret = [];
+      for (const sub_obj of obj) {
+        const value = getPathValueRec(sub_obj, idx + 1);
+        if (Array.isArray(value))
+          ret = ret.concat(value)
+        else
+          ret.push(value);
       }
-      return subpath_part_value[subpath_part];
-    }, object);
+      return ret;
+    }
+    return getPathValueRec(obj[curr_path], idx + 1);
+  };
+  return getPathValueRec(object, 1);
 }
 
 module.exports = getPathValue;
